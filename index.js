95a3ca7f9d9518e9c37e991d5e1fddaf8c69be31
Abhijeet Kumar <85831655+aryaabhii@users.noreply.github.com>
2024-06-04 05:21:36 +0000

    dev container

---
+++ index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+( async () => {
+
+    try {
+        mongoose.connect('mongodb://localhost:27017/gitsetup');
+        console.log('DB CONNECTED');
+
+        const onListining = () => {
+            console.log('Listining on PORT 5000');
+        }
+
+        app.listen(5000, onListining);
+
+
+    } catch(error) {
+        console.error('Error : ', error);
+    }
+
+})()
